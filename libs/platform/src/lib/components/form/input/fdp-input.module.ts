16607be140d388c5e0ad26b39c25345b4562bc5a
I505460 <lokanathan.k@sap.com>
2020-07-07 14:05:50 +0530

    Update fdp-input.module.ts
    removing alias name in form module

--- libs/platform/src/lib/components/form/input/fdp-input.module.ts
+++ libs/platform/src/lib/components/form/input/fdp-input.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { InputComponent } from './input.component';
 import { CommonModule } from '@angular/common';
-import { FormModule as FdFormModule } from '@fundamental-ngx/core';
-import { FormsModule as coreFormModule } from '@angular/forms';
+import { FormModule } from '@fundamental-ngx/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [InputComponent],
-    imports: [CommonModule, coreFormModule, FdFormModule],
+    imports: [CommonModule, FormModule, FormsModule],
     exports: [InputComponent]
 })
 export class PlatformInputModule {}
